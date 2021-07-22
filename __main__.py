4c4fbc61e9bbd188222d52d698676d645334908b
a_lesh_a <diadool@list.ru>
2021-07-22 15:57:47 +0500

    now stocks have userlist

--- __main__.py
+++ __main__.py
@@ -6,7 +6,7 @@ from modules import *
 
 def start(update, context):
     User.get_user(update.effective_chat.id, users)
-    reply_keyboard = [['Add securities', 'Check securities', 'Remove securities']]
+    reply_keyboard = [['Add securities', 'Check bag', 'Remove securities']]
     update.message.reply_text(
         'I\'m a bot, I can check securitie\'s price and tell you when the security will cost as much as you need)0))',
         reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
@@ -14,39 +14,45 @@ def start(update, context):
     return 1
 
 
-def request_stocks(update, context):
+def request_stocks_to_add(update, context):
     update.message.reply_text('Talk to me your securities. Please use this format (Name - Wanted price):\nName1 - 54\nName2 - 17')
     return 2
 
 
-def get_stocks(update, context):  #TODO
+def get_stocks_to_add(update, context):  #TODO
     user = User.get_user(update.effective_chat.id, users)
     for _ in re.findall(r'\w+\-\d+', update.message.text.replace(' ', '')):
         name, cost = _.split('-')
         stock = Stock(name, cost)
-        if not stock.is_it_correct():
-            update.message.reply_text(f'Mission failed on {stock.name}')
+        if not user.try_add_stock(stock):
+            update.message.reply_text(f'Mission failed on {stock.name}. Try again')
             return 2
-        user.try_add_stock(stock)
+    update.message.reply_text('Mission complete')
+    return ConversationHandler.END
+
+
+def request_stocks_to_remove(update, context):
+    update.message.reply_text('Talk to me your securities to remove. Please use this format (Name):\nName1\nName2')
+    return 3
+
+
+def get_stocks_to_remove(update, context):  #TODO
+    user = User.get_user(update.effective_chat.id, users)
+    for _ in re.findall(r'\w+', update.message.text.replace(' ', '')):
+        if not user.try_delete_stock(Stock(_)):
+            update.message.reply_text(f'Mission failed on {_}. No such stocks in your bag. Try again')
+            return 3
     update.message.reply_text('Mission complete')
     return ConversationHandler.END
 
 
 def show_bag(update, context):
     user = User.get_user(update.effective_chat.id, users)
-    output = 'Your securities to check:\n'
+    output = 'Your securities to check:\n' if user.stocks else 'Empty'
     for stock in user.stocks:
-        output += f'Name: {stock.name} and needed price: {stock.needed_price} \n'
+        output += f'Name: {stock.name} and needed price: {stock.needed_price}\n'
     update.message.reply_text(output)
     return ConversationHandler.END
-
-
-def show_price(update, context):  #TODO
-    return ConversationHandler.END
-
-
-def request_stock_name(update, context):  #TODO
-    return 3
 
 
 def cancel(update, context):
@@ -62,17 +68,18 @@ dp = updater.dispatcher
 conv_handler = ConversationHandler(
     entry_points=[
         CommandHandler('start', start),
-        CommandHandler('check_price', request_stock_name),
         CommandHandler('check_bag', show_bag),
-        CommandHandler('add_securities', request_stocks)
+        CommandHandler('add_securities', request_stocks_to_add),
+        CommandHandler('del_securities', request_stocks_to_remove)
     ],
     states={
         1 : [
-                MessageHandler(Filters.regex('Add securities'), request_stocks),
-                MessageHandler(Filters.regex('Check securitie\'s price'), request_stock_name)
+                MessageHandler(Filters.regex('Add securities'), request_stocks_to_add),
+                MessageHandler(Filters.regex('Check bag'), show_bag),
+                MessageHandler(Filters.regex('Remove securities'), request_stocks_to_remove)
             ],
-        2 : [MessageHandler(Filters.text, get_stocks)],
-        3 : [MessageHandler(Filters.text, show_price)]
+        2 : [MessageHandler(Filters.text, get_stocks_to_add)],
+        3 : [MessageHandler(Filters.text, get_stocks_to_remove)],
     },
     fallbacks=[CommandHandler('cancel', cancel)],
     allow_reentry=True
--- modules.py
+++ modules.py
@@ -5,23 +5,28 @@ class User:
     
 
     def try_add_stock(self, stock):
+        if not stock.is_it_correct():
+            return False
+
         is_contain = False
         for s in self.stocks:
             if stock.ticker == s.ticker:
                 s.needed_price = stock.needed_price
                 is_contain = True
                 break
+
         if not is_contain:
-            self.stocks.append(stock) 
+            stock.try_add_user(self)
+            self.stocks.append(stock)
+        return True
     
 
     def try_delete_stock(self, stock):
         for s in self.stocks:
             if stock.ticker == s.ticker:
-                self.stocks.pop(s)
-                break
-
-
+                self.stocks.remove(s)
+                return True
+        return False
 
 
     @staticmethod
@@ -35,10 +40,19 @@ class User:
             
 
 class Stock:
-    def __init__(self, name, price):
-        self.name = name
+    def __init__(self, name, price = 54):
+        self.name = name.upper()
         self.needed_price = price
         self.ticker = self.name  #TODO
+        self.users = []
+
+
+    def try_add_user(self, user):
+        for u in self.users:
+            if user.chat_id == u.chat_id:
+                return False
+        self.users.append(user)
+        return True
 
 
     def is_it_correct(self):
